dba05adfc9ef767b8da3a4e34f2472b8d1a51b5d
Maxcitmiv <maksmaks5213@gmail.com>
2026-05-09 21:35:56 +0300

    домашка mobile

--- tests/android_app/test_wikipedia.py
+++ tests/android_app/test_wikipedia.py
@@ -13,3 +13,17 @@ def test_search():
         results = browser.all((AppiumBy.ID, 'org.wikipedia.alpha:id/page_list_item_title'))
         results.should(have.size_greater_than(0))
         results.first.should(have.text('Appium'))
+
+def test_click():
+    browser.element((AppiumBy.ACCESSIBILITY_ID, "Search Wikipedia")).click()
+    browser.element((AppiumBy.ID, "org.wikipedia.alpha:id/search_src_text")).type('Xiaomi')
+    results = browser.all((AppiumBy.ID, 'org.wikipedia.alpha:id/page_list_item_title'))
+    results.should(have.size_greater_than(0))
+    results.first.should(have.text('Xiaomi'))
+    browser.element((
+        AppiumBy.ANDROID_UIAUTOMATOR,
+        'new UiSelector()'
+        '.resourceId("org.wikipedia.alpha:id/page_list_item_title")'
+        '.text("Xiaomi")'
+)).click()
+    
